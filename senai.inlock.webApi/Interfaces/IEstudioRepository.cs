3b59b353f0b0e64e852efd56f3b312b05f4d843e
Rebeca <rebecacarolina75@gmail.com>
2024-02-05 15:18:03 -0300

    InLock Games Project

---
+++ senai.inlock.webApi/Interfaces/IEstudioRepository.cs
@@ -0,0 +1,11 @@
+﻿using senai.inlock.webApi.Domains;
+
+namespace senai.inlock.webApi.Interfaces
+{
+    public interface IEstudioRepository
+    {
+        void Cadastrar(EstudioDomain novoEstudio);
+        List<EstudioDomain> ListarTodos();
+        void Deletar(int id);
+    }
+}
